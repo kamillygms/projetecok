64aaa550db9d86d58767282e0ab5a715a7330028
IFprojetec <89222307+IFprojetec@users.noreply.github.com>
2021-08-25 20:46:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 
+document.querySelector("#abrir-login").addEventListener("click", function(){
+    document.querySelector(".popup").classList.add("active");
+});
+document.querySelector(".popup .fechar").addEventListener("click",function(){
+    document.querySelector(".popup").classList.remove("active");
+});
+
+
+
+
+
+
 
 
